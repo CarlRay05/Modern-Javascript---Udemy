112ac56033edd9d310188d75373ca70e758054a9
Carl Ray <CarlRay05@gmail.com>
2019-05-23 16:47:58 -0500

    Update sandbox.js
    Lesson 40 object literals

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -168,25 +168,55 @@
 // const result = greet();
 // console.log(result);
 
-const bill = function(products, tax){
-  let total = 0;
-  for(let i = 0; i < products.length; i++){
-    total += products[i] + products[i] * tax;
-  }
-  return total;
-}
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
 
-const bill = (products, tax) => {
-	let total = 0;
-  	for(let i = 0; i < products.length; i++){
-    	total += products[i] + products[i] * tax;
-  	}
-  return total;
+// const bill = (products, tax) => {
+// 	let total = 0;
+//   	for(let i = 0; i < products.length; i++){
+//     	total += products[i] + products[i] * tax;
+//   	}
+//   return total;
+// };
+
+
+// // console.log(greet());
+// console.log(bill([10,15,30], 0.2));
+
+// object literals
+let user = {
+  name: 'crystal',
+  age: 30,
+  email: 'crystal@thenetninja.co.uk',
+  location: 'berlin',
+  blogs: ['why mac and cheese rules', '10 things to make with marmite'],
+  login(){
+    console.log('the user logged in');
+  },
+  logout(){
+    console.log('the user logged out');
+  },
+  logBlogs(){
+    // console.log(this.blogs);
+    console.log('this user has written the following blogs:');
+    this.blogs.forEach(blog => {
+      console.log(blog);
+    });
+  }
 };
 
+// user.login();
+// user.logout();
+user.logBlogs();
+console.log(this);
 
-// console.log(greet());
-console.log(bill([10,15,30], 0.2));
+
+
 
 
 
