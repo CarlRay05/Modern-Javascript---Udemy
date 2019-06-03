97959440bb1bb3d38e0a62db74eaf6e24959bced
Carl Ray <CarlRay05@gmail.com>
2019-06-03 16:48:35 -0500

    Update sandbox.js
    updates through lesson 40

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -189,31 +189,56 @@
 // console.log(bill([10,15,30], 0.2));
 
 // object literals
+
 let user = {
   name: 'crystal',
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
   blogs: ['why mac and cheese rules', '10 things to make with marmite'],
-  login(){
-    console.log('the user logged in');
+  login: function () {
+    console.log(('the user logged in'));
   },
-  logout(){
-    console.log('the user logged out');
+  logout: function () {
+    console.log(('the user logged out'));
   },
-  logBlogs(){
-    // console.log(this.blogs);
+  logBlogs: function(){
     console.log('this user has written the following blogs:');
     this.blogs.forEach(blog => {
       console.log(blog);
-    });
+    })
   }
 };
 
-// user.login();
-// user.logout();
 user.logBlogs();
 console.log(this);
+
+
+// user.login();
+// user.logout();
+
+// const name = 'mario';
+// name.toUpperCase();
+
+
+
+// console.log(user);
+// console.log(user.name); //dot notation
+
+// console.log(user['name']);  //bracket notation
+// console.log(user['email']); //bracket notation
+
+// user['name'] = 'chun-li;';  //bracket notation
+// console.log(user['name']);  //bracket notation
+
+// console.log(typeof user);
+
+
+
+
+
+
+
 
 
 
