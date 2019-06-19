6f5730084a07f76d5220f4120f3085c4af96a4bd
Carl Ray <CarlRay05@gmail.com>
2019-06-19 16:43:08 -0500

    Lesson 47 finished

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -262,18 +262,36 @@
 // console.log(userOne, userTwo);
 
 // **** Chapter 6 ****
-const para = document.querySelector('.error');
-console.log(para);
+// const para = document.querySelector('div.error');
+// console.log(para);
 
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
 
+// paras.forEach(para => {
+//     console.log(para);
+// });
 
+// console.log(errors);
 
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
 
+// // get an element by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
 
+// errors.forEach(errors => {
+//     console.log(error);
+    
+// });
 
-
-
-
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
 
 
 
