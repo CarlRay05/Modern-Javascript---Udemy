6b130ef2c4d704e606d3c24c628b4e92e1fc6318
Carl Ray <CarlRay05@gmail.com>
2019-09-13 14:13:29 -0500

    Lesson 71: Filter Method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,14 +1,24 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+// const scores = [10, 30, 15, 25, 50, 40, 5];
 
-const filteredScores = scores.filter((score) => {
-  return score > 20;
-});
+// const users = [
+//     { name: 'shaun', premium: true },
+//     { name: 'yoshi', premium: false },
+//     { name: 'mario', premium: false },
+//     { name: 'chun-li', premium: true },
+// ];
 
-console.log(filteredScores);
+// const premiumUsers = users.filter(user => user.premium); 
+
+// console.log(premiumUsers);
+
+// // const filteredScores = scores.filter((score) => { return score > 20; });
+// // const filteredScores = scores.filter((score) => score > 20);
+
+// // console.log(filteredScores);
 
 
-// ! Original 
-//const scores = [10, 30, 15, 25, 50, 40, 5];
+// ! Original
+// const scores = [10, 30, 15, 25, 50, 40, 5];
 
 // const highScores = scores.filter(score => score > 20);
 // console.log(highScores);
@@ -22,3 +32,6 @@ console.log(filteredScores);
 
 // const premiumUsers = users.filter(user => user.premium);
 // console.log(premiumUsers);
+
+
+
